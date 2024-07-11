bf668da4aca57bde74ac06908574b14545e5dd3d
Тарас Квик <Taras.Kvyk.PZ.2022@edu.lpnu.ua>
2024-07-12 00:53:23 +0300

    Added unit tests for ApplicationDbContext and ShortUrlRepository classes

---
+++ ShortenUrl1/ShortenUrl.Tests.xUnit/ApplicationDbContextTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShortenUrl.Domain.Entities;
+using ShortenUrl.Persistence;
+
+namespace ShortenUrl.Tests.xUnit;
+
+public class ApplicationDbContextTests
+{
+    [Fact]
+    public void CanInsertShortUrlIntoDatabase()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "test_database")
+            .Options;
+
+        using (var context = new ApplicationDbContext(options))
+        {
+            var shortUrl = new ShortUrl
+            {
+                OriginalUrl = "https://www.google.com/23456ytgfderf",
+                ShortenUrl = "https://www.google.com/abc",
+                CreatedByUserId = "1",
+                Description = "Google",
+                CreatedDate = DateTime.Now
+            };
+                
+            // Act
+            context.ShortUrls.Add(shortUrl);
+            context.SaveChanges();
+
+            // Assert
+            Assert.Single(context.ShortUrls.ToList());
+        }
+    }
+
+    [Fact]
+    public async Task CanRetrieveShortUrlByIdFromDatabase()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "test_database")
+            .Options;
+
+        using (var context = new ApplicationDbContext(options))
+        {
+            var shortUrl = new ShortUrl
+            {
+                OriginalUrl = "https://www.google.com/23456ytgfderf",
+                ShortenUrl = "https://www.google.com/abc",
+                CreatedByUserId = "1",
+                Description = "Google",
+                CreatedDate = DateTime.Now
+            };
+
+            context.ShortUrls.Add(shortUrl);
+            await context.SaveChangesAsync();
+
+            // Act
+            var retrievedShortUrl = await context.ShortUrls.FindAsync(shortUrl.Id);
+
+            // Assert
+            Assert.NotNull(retrievedShortUrl);
+            Assert.Equal(shortUrl.OriginalUrl, retrievedShortUrl.OriginalUrl);
+        }
+    }
+
+    [Fact]
+    public async Task CanRemoveShortUrlByIdFromDatabase()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "test_database")
+            .Options;
+
+        using (var context = new ApplicationDbContext(options))
+        {
+            var shortUrl = new ShortUrl
+            {
+                OriginalUrl = "https://www.google.com/23456ytgfderf",
+                ShortenUrl = "https://www.google.com/abc",
+                CreatedByUserId = "1",
+                Description = "Google",
+                CreatedDate = DateTime.Now
+            };
+
+            context.ShortUrls.Add(shortUrl);
+            await context.SaveChangesAsync();
+
+            // Act
+            context.ShortUrls.Remove(shortUrl);
+            await context.SaveChangesAsync();
+
+            // Assert
+            Assert.Empty(context.ShortUrls);
+        }
+    }
+}
---
+++ ShortenUrl1/ShortenUrl.Tests.xUnit/ShortUrlRepositoryTests.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShortenUrl.Domain.Entities;
+using ShortenUrl.Persistence;
+using ShortenUrl.Persistence.Repository.Implementations;
+
+namespace ShortenUrl.Tests.xUnit;
+
+public class ShortUrlRepositoryTests
+{
+    private readonly DbContextOptions<ApplicationDbContext> _options;
+
+    public ShortUrlRepositoryTests()
+    {
+        _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "test_database")
+            .Options;
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldReturnAllEntities()
+    {
+        // Arrange
+        using (var context = new ApplicationDbContext(_options))
+        {
+            context.ShortUrls.RemoveRange(context.ShortUrls);
+            await context.SaveChangesAsync();
+            
+            var repository = new Repository<ShortUrl>(context);
+            var expectedEntities = new List<ShortUrl>
+            {
+                new ShortUrl
+                {
+                    OriginalUrl = "https://www.google.com/23456ytgfderf",
+                    ShortenUrl = "https://www.google.com/abc",
+                    CreatedByUserId = "1",
+                    Description = "Google",
+                    CreatedDate = DateTime.Now
+                }
+            };
+            await context.ShortUrls.AddRangeAsync(expectedEntities);
+            await context.SaveChangesAsync();
+            
+            // Act
+            var actualEntities = await repository.GetAllAsync();
+
+            // Assert
+            Assert.Equal(expectedEntities.Count, actualEntities.Count());
+        }
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnEntityWithMatchingId()
+    {
+        // Arrange
+        using (var context = new ApplicationDbContext(_options))
+        {
+            var repository = new Repository<ShortUrl>(context);
+            var expectedEntity = new ShortUrl
+            {
+                OriginalUrl = "https://www.google.com/23456ytgfderf",
+                ShortenUrl = "https://www.google.com/abc",
+                CreatedByUserId = "1",
+                Description = "Google",
+                CreatedDate = DateTime.Now
+            };
+            await context.ShortUrls.AddAsync(expectedEntity);
+            await context.SaveChangesAsync();
+            
+            // Act
+            var actualEntity = await repository.GetByIdAsync(1);
+
+            // Assert
+            Assert.Equal(expectedEntity.OriginalUrl, actualEntity.OriginalUrl);
+        }
+    }
+
+    [Fact]
+    public async Task AddAsync_ShouldAddNewEntity()
+    {
+        // Arrange
+        using (var context = new ApplicationDbContext(_options))
+        {
+            var repository = new Repository<ShortUrl>(context);
+            var entityToAdd = new ShortUrl
+            {
+                OriginalUrl = "https://www.google.com/23456ytgfderf",
+                ShortenUrl = "https://www.google.com/abc",
+                CreatedByUserId = "1",
+                Description = "Google",
+                CreatedDate = DateTime.Now
+            };
+
+            // Act
+            var result = await repository.AddAsync(entityToAdd);
+
+            // Assert
+            Assert.True(result);
+
+            var addedEntity = await context.ShortUrls.FirstOrDefaultAsync(u => u.OriginalUrl == "https://www.google.com/23456ytgfderf");
+            Assert.NotNull(addedEntity);
+        }
+    }
+
+    [Fact]
+    public async Task RemoveByIdAsync_ShouldRemoveEntityWithMatchingId()
+    {
+        // Arrange
+        using (var context = new ApplicationDbContext(_options))
+        {
+            var repository = new Repository<ShortUrl>(context);
+            var entityToRemove = new ShortUrl
+            {
+                OriginalUrl = "https://www.google.com/23456ytgfderf",
+                ShortenUrl = "https://www.google.com/abc",
+                CreatedByUserId = "1",
+                Description = "Google",
+                CreatedDate = DateTime.Now
+            };
+            
+            await context.ShortUrls.AddAsync(entityToRemove);
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await repository.RemoveByIdAsync(1);
+
+            // Assert
+            Assert.True(result);
+
+            var removedEntity = await context.ShortUrls.FindAsync(1);
+            Assert.Null(removedEntity);
+        }
+    }
+    
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnNullForNonExistingId()
+    {
+        // Arrange
+        using (var context = new ApplicationDbContext(_options))
+        {
+            var repository = new Repository<ShortUrl>(context);
+
+            // Act
+            var actualEntity = await repository.GetByIdAsync(999); 
+
+            // Assert
+            Assert.Null(actualEntity);
+        }
+    }
+    
+    [Fact]
+    public async Task RemoveByIdAsync_ShouldReturnFalseForNonExistingId()
+    {
+        // Arrange
+        using (var context = new ApplicationDbContext(_options))
+        {
+            var repository = new Repository<ShortUrl>(context);
+
+            // Act
+            var result = await repository.RemoveByIdAsync(999); 
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
